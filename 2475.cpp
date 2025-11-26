d963acbf5dfb1ec947e5f238ded9b0073c40fb55
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 04:06:11 +0900

    Create 2475.cpp

---
+++ 2475.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i, in, ans=0;
+
+    for(i=0;i<5;i++) {
+        scanf("%d", &in);
+        ans+=in*in;
+    }
+
+    printf("%d\n", ans % 10);
+    return 0;
+}
