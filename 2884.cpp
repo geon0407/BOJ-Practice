11f27c1b0af5ec05f702851418705e9268c77502
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:13:26 +0900

    Create 2884.cpp

---
+++ 2884.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int h, m;
+    scanf("%d%d", &h, &m);
+
+    int ansh, ansm;
+    if(m < 45) {
+        ansm = m + 15;
+        ansh = h - 1;
+        if(ansh < 0) {
+            ansh = 23;
+        }
+    }
+    else {
+        ansm = m - 45;
+        ansh = h;
+    }
+
+    printf("%d %d\n", ansh, ansm);
+    return 0;
+}
