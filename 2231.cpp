774fc4abc12bee8b01fd4f21820a1063de7e7597
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:18:24 +0900

    Create 2231.cpp

---
+++ 2231.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int f();
+
+int n;
+
+int main(void)
+{
+    scanf("%d", &n);
+    int res = f();
+
+    if(res == n) {
+        printf("0\n");
+    }
+    else {
+        printf("%d\n", res);
+    }
+
+    return 0;
+}
+
+int f()
+{
+    int res = 1;
+
+    while( 1 ) {
+        int c = res;
+        int sum = res;
+
+        while( c ) {
+            sum += c % 10;
+            c /= 10;
+        }
+
+        if(res == n || sum == n) {
+            break;
+        }
+        res++;
+    }
+
+    return res;
+}
