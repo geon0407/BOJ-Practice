7b9c4a9fa82764c100727f4c0a40bcd1986cb020
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:31:16 +0900

    Create 1929.cpp

---
+++ 1929.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int num[1000010];
+
+int main(void)
+{
+    int i, j, m, n;
+    scanf("%d%d", &m, &n);
+
+    for(i=2;i<=n;i++) {
+        for(j=i;j<=n;j+=i) {
+            num[j]++;
+        }
+    }
+
+    for(i=m;i<=n;i++) {
+        //printf("%d\n", num[i]);
+        if(num[i] == 1) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
