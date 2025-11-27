3b9ac54ea6301ffc3c735971635c206ccd32ec37
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 22:48:24 +0900

    Create 11047.cpp - 동전 0

---
+++ 11047.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int money[15];
+
+int main(void)
+{
+    int N, K, i, cursor, cnt=0;
+    scanf("%d %d", &N, &K);
+    cursor = N-1;
+
+    for(i=0;i<N;i++) {
+        scanf("%d", &money[i]);
+    }
+
+    while( K != 0 ) {
+        for(i=cursor;i>=0;i--) {
+            if(K >= money[i]) {
+                K -= money[i];
+                cursor = i;
+                cnt++;
+                break;
+            }
+        }
+    }
+
+    printf("%d\n", cnt);
+    return 0;
+}
