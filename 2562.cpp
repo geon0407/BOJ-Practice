23d502c78e216d0f12fd8435b747fbd3ee2e839c
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:13:04 +0900

    Create 2562.cpp

---
+++ 2562.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i, in, ans_fig=0, ans_num=0;
+    for(i=0;i<9;i++) {
+        scanf("%d", &in);
+        if(ans_fig < in) {
+            ans_fig = in;
+            ans_num = i+1;
+        }
+    }
+
+    printf("%d\n%d\n", ans_fig, ans_num);
+    return 0;
+}
