5292a5bc681fca82c0dc32be54b8fbc998393ef9
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:29:59 +0900

    Create 10828.cpp

---
+++ 10828.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(void)
+{
+    stack<int> st;
+    string command;
+    int i, n;
+    cin >> n;
+
+    for(i=0;i<n;i++) {
+        cin >> command;
+
+        if(command == "push") {
+            int num;
+            cin >> num;
+            st.push(num);
+        }
+        else if(command == "pop") {
+            if(!st.empty()) {
+                cout << st.top() << endl;
+                st.pop();
+            }
+            else {
+                cout << "-1" << endl;
+            }
+        }
+        else if(command == "size") {
+            cout << st.size() << endl;
+        }
+        else if(command == "empty") {
+            if(st.empty()) {
+                cout << "1" << endl;
+            }
+            else {
+                cout << "0" << endl;
+            }
+        }
+        else if(command == "top") {
+            if(!st.empty()) {
+                cout << st.top() << endl;
+            }
+            else {
+                cout << "-1" << endl;
+            }
+        }
+    }
+
+    return 0;
+}
