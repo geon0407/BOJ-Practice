7af621524fa2b521edbe3c45bfcb34273087adde
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:25:21 +0900

    Create 2751.cpp

---
+++ 2751.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+int arr[1001010];
+int N;
+
+int main(void)
+{
+    int i;
+    scanf("%d", &N);
+
+    for(i=0;i<N;i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    sort(arr, arr+N);
+
+    for(i=0;i<N;i++) {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
