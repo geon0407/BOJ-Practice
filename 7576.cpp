d26ba8e0045468e87f241be7458c8ed01af8ce84
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 23:13:31 +0900

    토마토 (고등부)
    2013 한국정보올림피아드 시, 도 지역본선 고등부 1번

---
+++ 7576.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+int tomato[1001][1001];
+int d[1001][1001];
+int n, m;
+
+int main(void) 
+{
+    scanf("%d %d", &m, &n);
+    queue<pair<int, int> > q;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            scanf("%d", &tomato[i][j]);
+            d[i][j] = -1;
+            if (tomato[i][j] == 1) {
+                q.push({i, j});
+                d[i][j] = 0;
+            }
+        }
+    }
+    
+    while (!q.empty()) {
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                if (tomato[nx][ny] == 0 && d[nx][ny] == -1) { 
+                    d[nx][ny] = d[x][y] + 1;
+                    q.push({nx, ny});
+                }
+            }
+        }
+    }
+    
+    int result = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            result = max(result,d[i][j]);
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (tomato[i][j] == 0 && d[i][j] == -1) {
+                result = -1;
+            }
+        }
+    }
+    
+    cout << result;
+    return 0;
+}
