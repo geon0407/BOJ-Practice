d7c2f5921cf8c8bcda009264108979a40aacced4
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:21:51 +0900

    Create 1546.cpp

---
+++ 1546.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+double score[1010];
+
+int main(void)
+{
+    double av=0, max_score=0;
+    int n, i;
+    scanf("%d", &n);
+    for(i=0;i<n;i++) {
+        scanf("%lf", &score[i]);
+        max_score = max(max_score, score[i]);
+    }
+
+    for(i=0;i<n;i++) {
+        score[i] = score[i] / max_score * 100;
+        av += score[i];
+    }
+
+    printf("%.2lf\n", av / n);
+    return 0;
+}
