055411430d97d710c21fd12c2d478dbcc9461bba
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 15:58:34 +0900

    Create 10171.cpp

---
+++ 10171.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    printf("\\    /\\\n");
+    printf(" )  ( ') \n");
+    printf("(  /  ) \n");
+    printf(" \\(__)|\n");
+    return 0;
+}
