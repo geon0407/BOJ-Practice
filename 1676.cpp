d598925154814d80cdd149617d3a8e1490cb90c2
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:25:04 +0900

    Create 1676.cpp

---
+++ 1676.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n, m5, m25, m125;
+    scanf("%d", &n);
+    m5 = n / 5;
+    m25 = n / 25;
+    m125 = n / 125;
+    printf("%d\n", m5 + m25 + m125);
+    return 0;
+}
