2afd33c1fd113d31e36549b53a768a44b6e7f32b
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 22:47:13 +0900

    Create 1620.cpp - 나는야 포켓몬 마스터 이다솜

---
+++ 1620.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<string, int> name;
+string num[100010];
+
+int n, m;
+
+int main(void)
+{
+    int i;
+    scanf("%d%d", &n, &m);
+    for(i=0;i<n;i++) {
+        string s;
+        cin >> s;
+        name.insert(pair<string, int>(s, i+1));
+        num[i] = s;
+    }
+
+    for(i=0;i<m;i++) {
+        char str[25];
+        scanf("%s", str);
+
+        if(isdigit(str[0])) {
+            cout << num[atoi(str)-1] << "\n";
+        }
+        else {
+            string tmp = str;
+            printf("%d\n", name[tmp]);
+        }
+    }
+
+    return 0;
+}
