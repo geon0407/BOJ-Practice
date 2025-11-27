4e11ab2dbce3340a2bf4e5160b88ca8b15cc66db
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:17:05 +0900

    Create 4153.cpp

---
+++ 4153.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int l[3];
+
+int main(void)
+{
+    while( 1 ) {
+        scanf("%d%d%d", &l[0], &l[1], &l[2]);
+        sort(l, l+3);
+        if(l[2] == 0) {
+            exit(0);
+        }
+        if(l[2]*l[2] == (l[0]*l[0] + l[1]*l[1])) {
+            printf("right\n");
+        }
+        else {
+            printf("wrong\n");
+        }
+    }
+    return 0;
+}
