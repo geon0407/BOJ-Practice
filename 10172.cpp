b142acc90b432f0aa981eb92e9ae7bdc7044b5f9
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 15:59:06 +0900

    Create 10172.cpp

---
+++ 10172.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"\`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}
