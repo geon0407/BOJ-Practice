22d0179da9631bf12601f3954e007c955bf029af
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:13:49 +0900

    Create 10250.cpp

---
+++ 10250.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int t, h, w, n, i;
+    scanf("%d", &t);
+
+    for(i=0;i<t;i++) {
+        scanf("%d%d%d", &h, &w, &n);
+        if(n % h) {
+            if(n / h + 1 > 9) {
+                printf("%d%d\n", n%h, n/h+1);
+            }
+            else {
+                printf("%d0%d\n", n%h, n/h+1);
+            }
+        }
+        else {
+            if(n / h > 9) {
+                printf("%d%d\n", h, n/h);
+            }
+            else {
+                printf("%d0%d\n", h, n/h);
+            }
+        }
+    }
+
+    return 0;
+}
