825353bcf3bf4ab6b52dc3be1a78b2761ca08f17
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:30:23 +0900

    Create 10845.cpp

---
+++ 10845.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+queue<int> data;
+
+int main(void)
+{
+    int n, i;
+    scanf("%d", &n);
+    for(i=0;i<n;i++) {
+        char com[10];
+        scanf("%s", com);
+        if(strcmp(com, "push") == 0) {
+            int in;
+            scanf("%d", &in);
+            data.push(in);
+        }
+        else if(strcmp(com, "pop") == 0) {
+            if(data.empty()) {
+                printf("-1\n");
+            }
+            else {
+                printf("%d\n", data.front());
+                data.pop();
+            }
+        }
+        else if(strcmp(com, "size") == 0) {
+            printf("%d\n", data.size());
+        }
+        else if(strcmp(com, "empty") == 0) {
+            printf("%d\n", data.empty());
+        }
+        else if(strcmp(com, "front") == 0) {
+            if(data.empty()) {
+                printf("-1\n");
+            }
+            else {
+                printf("%d\n", data.front());
+            }
+        }
+        else {
+            if(data.empty()) {
+                printf("-1\n");
+            }
+            else {
+                printf("%d\n", data.back());
+            }
+        }
+    }
+
+    return 0;
+}
