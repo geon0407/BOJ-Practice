6a627a9f136010b5382d0036eac8f9c8299bbf47
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:26:55 +0900

    Create 1920.cpp

---
+++ 1920.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    vector<int> v;
+    int n;
+
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) {
+        int in;
+        scanf("%d", &in);
+        v.push_back(in);
+    }
+    sort(v.begin(), v.end());
+
+    scanf("%d", &n);
+    for(int i=0;i<n;i++) {
+        int in;
+        scanf("%d", &in);
+        printf("%d\n", binary_search(v.begin(), v.end(), in));
+    }
+
+    return 0;
+}
