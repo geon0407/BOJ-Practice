f9fa4083a56562d992c8afc665bbbaf2ba119b8f
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 22:54:54 +0900

    구간 합 구하기 4

---
+++ 11659.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,m;
+int arr[100005];
+
+int main()
+{
+    scanf("%d%d",&n,&m);
+    for(int i=1;i<=n;++i){
+        scanf("%d",&arr[i]);
+        arr[i]+=arr[i-1];
+    }
+    
+    for(int i=0;i<m;++i){
+        int x, y;
+        scanf("%d%d",&x,&y);
+        if(x > y) {
+            swap(x,y);
+        }
+        printf("%d\n",arr[y]-arr[x-1]);
+    }
+    
+    return 0;
+}
