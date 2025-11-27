71465d2a29f4422742285a3be64e80dd0eb5e6e4
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:08:26 +0900

    Create 10871.cpp

---
+++ 10871.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int N, X;
+
+int main(void)
+{
+	int i, number;
+	scanf("%d %d", &N, &X);
+	
+	for(i=0;i<N;i++) {
+		scanf("%d", &number);
+		if(number < X) {
+			printf("%d ", number);
+		}
+	}
+	
+	return 0;
+}
