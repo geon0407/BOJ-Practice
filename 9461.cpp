1e00717b3fd87c1b2af8b385eaf8939a7c8dad9b
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 22:54:06 +0900

    파도반 수열

---
+++ 9461.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long dp[110] = {0, 1, 1, 1, 2, 2, };
+
+int main(void)
+{
+    int i;
+    for(i=6;i<=100;i++) {
+        dp[i] = dp[i-1] + dp[i-5];
+    }
+
+    int tc;
+    scanf("%d", &tc);
+    while(tc--) {
+        int in;
+        scanf("%d", &in);
+        printf("%lld\n", dp[in]);
+    }
+    return 0;
+}
