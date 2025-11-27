7f4ac7d98197e49a6c4c6862b29750491909a9c2
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:29:19 +0900

    Create 9012.cpp

---
+++ 9012.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool f(string s);
+
+int main(void)
+{
+    int t;
+    scanf("%d", &t);
+
+    while(t--) {
+        string s;
+        cin >> s;
+        cout << (f(s) ? "YES\n" : "NO\n");
+    }
+
+    return 0;
+}
+
+bool f(string s)
+{
+    stack<char> st;
+
+    for(int i=0;i<s.size();i++) {
+        if(s[i] == '(') {
+            st.push(s[i]);
+        }
+        else {
+            if(st.empty()) {
+                return false;
+            }
+
+            st.pop();
+        }
+    }
+
+    if(st.empty()) {
+        return true;
+    }
+    return false;
+}
