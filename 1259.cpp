066aeeb863ba15852a59d799742c34e882d79f26
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:20:58 +0900

    Create 1259.cpp

---
+++ 1259.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    while( 1 ) {
+        string num;
+        bool flag = true;
+        cin >> num;
+        if(num == "0") {
+            exit(0);
+        }
+
+        for(int i=0;i<num.size()/2;i++) {
+            if(num[i] != num[num.size()-i-1]) {
+                flag = false;
+                break;
+            }
+        }
+
+        if(flag) {
+            cout << "yes" << endl;
+        }
+        else {
+            cout << "no" << endl;
+        }
+    }
+
+    return 0;
+}
