1f8449c9c1d7898e3fe1f49febd1caf1d12db72a
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:09:38 +0900

    Create 10952.cpp

---
+++ 10952.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int n1, n2;
+    while( 1 ) {
+        scanf("%d%d", &n1, &n2);
+        if(n1 + n2 == 0) {
+            break;
+        }
+        printf("%d\n", n1+n2);
+    }
+    return 0;
+}
