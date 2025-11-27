78b0cc98e4f4281e676bc8accfa689325b74aa55
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 22:56:29 +0900

    2×n 타일링 2

---
+++ 11727.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int DP[1010];
+
+int main(void)
+{
+    int n, i;
+    scanf("%d", &n);
+
+    DP[0] = DP[1] = 1;
+    for(i=2;i<=n;i++) {
+        DP[i] = (DP[i-1] + DP[i-2] * 2) % 10007;
+    }
+
+    printf("%d\n", DP[n] % 10007);
+    return 0;
+}
