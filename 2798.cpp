1c5edc47bde9805f11c10e5fcb8f53dca977c60e
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:19:18 +0900

    Create 2798.cpp

---
+++ 2798.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int card[110];
+int n, m;
+
+int main(void)
+{
+    int i, j, k, ans=0;
+    scanf("%d%d", &n, &m);
+    for(i=0;i<n;i++) {
+        scanf("%d", &card[i]);
+    }
+
+    for(i=0;i<n;i++) {
+        for(j=0;j<n;j++) {
+            for(k=0;k<n;k++) {
+                if(i == j || j == k || k == i) {
+                    continue;
+                }
+
+                int sum = card[i] + card[j] + card[k];
+                if(sum > m) {
+                    continue;
+                }
+                ans = max(ans, sum);
+            }
+        }
+    }
+
+    printf("%d\n", ans);
+    return 0;
+}
