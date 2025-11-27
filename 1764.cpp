5e4cf0000e43650e2a8fa20af1d461aa4357f92a
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 22:47:44 +0900

    Create 1764.cpp - 듣보잡

---
+++ 1764.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+map<string, int> l;
+vector<string> ans;
+
+int main(void)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n, m;
+    cin >> n >> m;
+
+    for(int i=0;i<n+m;i++) {
+        string str;
+        cin >> str;
+
+        l[str]++;
+        if(l[str] > 1) {
+            ans.push_back(str);
+        }
+    }
+    sort(ans.begin(), ans.end());
+
+    cout << ans.size() << endl;
+    for(auto i : ans) {
+        cout << i << endl;
+    }
+    return 0;
+}
