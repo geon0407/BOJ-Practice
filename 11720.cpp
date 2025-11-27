f11242ab1299c846fb5f6be5443b5a35d9b83ebc
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:12:40 +0900

    Create 11720.cpp

---
+++ 11720.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int i, in, cnt=0;
+    scanf("%d", &i);
+
+    for(i;i>=1;i--) {
+        scanf("%1d", &in);
+        cnt+=in;
+    }
+
+    printf("%d\n", cnt);
+    return 0;
+}
