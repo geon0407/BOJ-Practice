6f03f81b3604a07a20a7effea0f6278d015f93ec
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 23:04:18 +0900

    연결 요소의 개수

---
+++ 11724.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int find_root(int x);
+void union_tree(int x, int y);
+
+int root[1010];
+bool chk[1010];
+
+int main(void)
+{
+    int i, n, m, ans=0;
+    scanf("%d%d", &n, &m);
+    for(i=1;i<=n;i++) {
+        root[i] = i;
+    }
+
+    while(m--) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        union_tree(a, b);
+    }
+
+    for(i=1;i<=n;i++) {
+        int tmp = find_root(i);
+        if(chk[tmp] == false) {
+            chk[tmp] = true;
+            ans++;
+        }
+    }
+
+    printf("%d\n", ans);
+    return 0;
+}
+
+int find_root(int x)
+{
+    if(root[x] == x) {
+        return x;
+    }
+    else {
+        return root[x] = find_root(root[x]);
+    }
+}
+
+void union_tree(int x, int y)
+{
+    x = find_root(x);
+    y = find_root(y);
+
+    root[y] = x;
+
+    return ;
+}
