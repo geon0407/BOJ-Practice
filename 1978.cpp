d71cb35b47620513f026a3e30a6c2fd37eb3cca0
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 20:17:30 +0900

    Create 1978.cpp

---
+++ 1978.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+int check(int number);
+
+int main(void)
+{
+    int n, num, i, ans=0;
+    scanf("%d", &n);
+
+    for(i=0;i<n;i++) {
+        scanf("%d", &num);
+        ans += check(num);
+    }
+
+    printf("%d\n", ans);
+    return 0;
+}
+
+int check(int number)
+{
+    int i;
+    if(number == 1) {
+        return 0;
+    }
+
+    for(i=2;i<=number-1;i++) {
+        if(number % i == 0) {
+            return 0;
+        }
+    }
+
+    return 1;
+}
