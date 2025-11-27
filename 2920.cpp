b6a02e9db7412efd7bda14148f80622541fb2d2c
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:16:15 +0900

    Create 2920.cpp

---
+++ 2920.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+    int i, index=0, in, flag=0;
+    scanf("%d", &in);
+    if(in == 1) {
+        index = 2;
+        flag = 1;
+        for(i=0;i<7;i++) {
+            scanf("%d", &in);
+            if(in != index) {
+                printf("mixed\n");
+                exit(0);
+            }
+            index += flag;
+        }
+        printf("ascending\n");
+        exit(0);
+    }
+    else if(in == 8) {
+        index = 7;
+        flag = -1;
+        for(i=0;i<7;i++) {
+            scanf("%d", &in);
+            if(in != index) {
+                printf("mixed\n");
+                exit(0);
+            }
+            index += flag;
+        }
+        printf("descending\n");
+        exit(0);
+    }
+    else {
+        printf("mixed\n");
+        exit(0);
+    }
+    return 0;
+}
