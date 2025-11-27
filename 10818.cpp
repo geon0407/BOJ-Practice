c4bf631255b43575345c949f3f1abeaf7f995112
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 16:14:15 +0900

    Create 10818.cpp

---
+++ 10818.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    int i, n, ans_min=1000001, ans_max=-1000001;
+    scanf("%d", &n);
+    for(i=1;i<=n;i++) {
+        int in;
+        scanf("%d", &in);
+        ans_min = min(ans_min, in);
+        ans_max = max(ans_max, in);
+    }
+    printf("%d %d\n", ans_min, ans_max);
+    return 0;
+}
