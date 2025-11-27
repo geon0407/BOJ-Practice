6579f5152327d59cedfa5396d8892758d99171d3
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-27 22:07:53 +0900

    Create 10773.cpp - 제로

---
+++ 10773.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    stack<int> st;
+    int k;
+    scanf("%d", &k);
+
+    for(int i=0;i<k;i++) {
+        int in;
+        scanf("%d", &in);
+
+        if(in) {
+            st.push(in);
+        }
+        else {
+            st.pop();
+        }
+    }
+
+    int ans = 0;
+    while(!st.empty()) {
+        ans += st.top();
+        st.pop();
+    }
+
+    printf("%d\n", ans);
+    return 0;
+}
