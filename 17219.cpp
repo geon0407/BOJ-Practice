9d42cd2b9b52b91dbb8cea54bfb7887c8af81ec0
김건형 <53281908+geon0407@users.noreply.github.com>
2025-11-29 01:52:37 +0900

    비밀번호 찾기

---
+++ 17219.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    map<string, string> pw;
+    int n, m;
+    cin >> n >> m;
+
+    while(n--) {
+        string url, p;
+        cin >> url >> p;
+        pw.insert({url, p});
+    }
+
+    while(m--) {
+        string cmd;
+        cin >> cmd;
+        cout << pw[cmd] << "\n";
+    }
+
+    return 0;
+}
