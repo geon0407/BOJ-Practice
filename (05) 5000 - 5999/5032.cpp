78aef5b17e5fa36119a8b7306689844a5f1c8a5c
김건형 <53281908+geon0407@users.noreply.github.com>
2025-12-02 21:07:59 +0900

    Create 5032.cpp

---
+++ (05) 5000 - 5999/5032.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cout.tie(NULL);
+    cin.tie(NULL);
+
+    int e, f, c;
+    cin >> e >> f >> c;
+
+    int can = e+f, ans = 0;
+    while(can >= c) {
+        ans += can / c;
+        can = (can / c) + can % c;
+    }
+
+    cout << ans;
+    return 0;
+}
